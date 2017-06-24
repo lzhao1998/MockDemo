d9aca4ab2bda7a328c197d5cd9124a9563703989
Lai Zhe Hao <lzhao1998@gmail.com>
2017-06-24 12:55:53 +0800

    Created MockDemo Project

---
+++ src/simplemock.h
@@ -0,0 +1,5 @@
+#ifndef _SIMPLEMOCK_H
+#define _SIMPLEMOCK_H
+
+
+#endif // _SIMPLEMOCK_H
---
+++ test/test_simplemock.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "simplemock.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_simplemock_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement simplemock");
+}
